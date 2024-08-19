c81504b820ad80817562db6beb8f4b9b6f30b870
kcalvinroy <kcalvinroy@gmail.com>
2024-08-19 20:45:25 +0300

    Add video Block

--- src/lib/slices/VideoBlock/index.svelte
+++ src/lib/slices/VideoBlock/index.svelte
@@ -7,13 +7,13 @@
 </script>
 
 <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	<Heading tag="h2" size="lg">
+	<Heading tag="h2" size="sm">
 		{slice.primary.video_label}
 	</Heading>
 	<iframe
 		width="560"
 		height="315"
-		src={slice.primary.video_link}
+		src={slice.primary.video_link.toString()}
 		title={slice.primary.video_label}
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 		allowfullscreen
