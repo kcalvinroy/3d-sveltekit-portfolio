2a12cc7257e8c9444a0fcac3774171f7abce8eb0
kcalvinroy <kcalvinroy@gmail.com>
2024-08-19 21:37:50 +0300

    Add video Block-edit-11

--- src/lib/slices/VideoBlock/index.svelte
+++ src/lib/slices/VideoBlock/index.svelte
@@ -7,6 +7,21 @@
 </script>
 
 <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<div class="card">
+		<iframe
+			class="card-img"
+			width="800"
+			height="450"
+			src={slice.primary.video_link}
+			title={slice.primary.video_label}
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="origin"
+			allowfullscreen
+		></iframe>
+		<h2 class="card-title">{slice.primary.video_label}</h2>
+		<p class="card-text">{slice.primary.video_description}</p>
+	</div>
 	<Heading tag="h2" size="sm">
 		{slice.primary.video_label}
 	</Heading>
