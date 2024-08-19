f266fb674b1dcbe60354e600c6c9fe675aede1bd
kcalvinroy <kcalvinroy@gmail.com>
2024-08-19 20:48:28 +0300

    Add video Block

--- src/lib/slices/VideoBlock/index.svelte
+++ src/lib/slices/VideoBlock/index.svelte
@@ -10,13 +10,16 @@
 	<Heading tag="h2" size="sm">
 		{slice.primary.video_label}
 	</Heading>
-	<iframe
-		width="560"
-		height="315"
-		src={slice.primary.video_link.toString()}
-		title={slice.primary.video_label}
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-		allowfullscreen
-		class="w-full h-full"
-	></iframe>
+	<div class="aspect-w-16 aspect-h-9">
+		<iframe
+			width="853"
+			height="480"
+			src="https://www.youtube.com/embed/9yIBuGwH2a0"
+			title="Imagine Dragons - In Your Corner (Live) | Vevo Extended Play"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen
+		></iframe>
+	</div>
 </Bounded>
