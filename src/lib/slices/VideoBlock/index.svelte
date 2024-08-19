7467e5758f9ff91a93251cc094903fab80f76007
kcalvinroy <kcalvinroy@gmail.com>
2024-08-19 22:52:50 +0300

    Added video Block Formatting

--- src/lib/slices/VideoBlock/index.svelte
+++ src/lib/slices/VideoBlock/index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Content } from '@prismicio/client';
+	import { type Content, isFilled } from '@prismicio/client';
 	import Bounded from '$lib/components/Bounded.svelte';
 	import Heading from '$lib/components/Heading.svelte';
 	import { PrismicRichText } from '@prismicio/svelte';
@@ -8,25 +8,21 @@
 </script>
 
 <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	<div class="max-w-sm bg-white rounded-lg shadow-md">
-		<div class="relative pb-4/5">
-			<iframe
-				class="absolute w-full h-full rounded-t-lg"
-				width="800"
-				height="450"
-				src={slice.primary.video_link}
-				title={slice.primary.video_label}
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				referrerpolicy="origin"
-				allowfullscreen
-			></iframe>
-		</div>
-		<div class="p-4">
-			<h3 class="text-lg font-bold mb-2">{slice.primary.video_label}</h3>
-			<p class="text-gray-600">
-				<PrismicRichText field={slice.primary.video_description} />
-			</p>
-		</div>
+	<div class="flex flex-col items-center relative pb-4 rounded-lg shadow-lg">
+		<Heading size="sm" class="text-lg font-bold mb-2">{slice.primary.video_label}</Heading>
+		<iframe
+			class="relative max-w-full max-h-auto rounded-t-lg shadow-md"
+			width="800"
+			height="450"
+			src={slice.primary.video_link}
+			title={slice.primary.video_label}
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="origin"
+			allowfullscreen
+		></iframe>
+		{#if isFilled.richText(slice.primary.video_description)}
+			<p><PrismicRichText field={slice.primary.video_description} /></p>
+		{/if}
 	</div>
 </Bounded>
