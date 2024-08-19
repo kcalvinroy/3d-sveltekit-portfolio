cb820b5acf0d101c0a024fae1f35116e96376b19
kcalvinroy <kcalvinroy@gmail.com>
2024-08-19 20:30:33 +0300

    Add video Blockk

--- src/lib/slices/VideoBlock/index.svelte
+++ src/lib/slices/VideoBlock/index.svelte
@@ -6,4 +6,11 @@
 
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
+	<embed
+		src={slice.primary.video_link}
+		title={slice.primary.video_label}
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+		class="w-full h-full"
+	/>
 </section>
