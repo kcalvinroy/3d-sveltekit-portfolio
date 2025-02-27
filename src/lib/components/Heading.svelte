c796bcd4cd167a8db9fc0de260d0d0ed836b09c8
kcalvinroy <kcalvinroy@gmail.com>
2025-02-27 23:44:13 +0300

    Fixed projects display - responsive

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import clsx from 'clsx';
 
-	export let tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' = 'h2';
+	export let tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' = 'h1';
 	export let size: 'xl' | 'lg' | 'md' | 'sm' = 'lg';
 	let className: string = '';
 	export { className as class };
@@ -12,9 +12,9 @@
 	class={clsx(
 		'font-bold leading-tight tracking-tight  text-slate-300',
 		size === 'xl' && 'text-7xl md:text-9xl',
-		size === 'lg' && 'text-6xl md:text-6xl',
-		size === 'md' && 'text-3xl md:text-4xl',
-		size === 'sm' && 'text-xl md:text-2xl',
+		size === 'lg' && 'text-6xl md:text-8xl',
+		size === 'md' && 'text-5xl md:text-6xl',
+		size === 'sm' && 'text-3xl md:text-4xl',
 		className
 	)}
 >
