3784f019ef191dd8c55ce48aa0727a17172e0daf
kcalvinroy <kcalvinroy@gmail.com>
2025-02-27 23:42:37 +0300

    Fixed projects display - responsive

--- src/lib/components/ContentBody.svelte
+++ src/lib/components/ContentBody.svelte
@@ -14,12 +14,15 @@
 
 <Bounded tag="article">
 	<div class="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
-		<Heading tag="h1">{page.data.title}</Heading>
-		<div class="flex gap-4 text-yellow-400">
+		<!-- <Heading tag="h2">{page.data.title}</Heading> -->
+		<h2
+			class="flex flex-wrap font-bold text-3xl md:text-5xl leading-tight tracking-tight text-slate-300"
+		>
+			{page.data.title}
+		</h2>
+		<div class="pt-2 flex flex-wrap gap-4 text-yellow-400">
 			{#each page.tags as tag}
-				<span class="text-xl font-bold">
-					{tag}
-				</span>
+				<span class="text-sm sm:text-base font-semibold">{tag}</span>
 			{/each}
 		</div>
 		<p class="mt-8 border-b border-slate-600 text-xl font-medium text-slate-300">{formattedDate}</p>
